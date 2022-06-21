3dfaf97258efde1558c02e8b6661cbd4c81741b0
andres alcocer <devandres.tech@gmail.com>
2022-06-21 17:49:48 -0400

    completed basic functionality

--- mystery_word.py
+++ mystery_word.py
@@ -1,4 +1,5 @@
 from tkinter import W
+import random
 
 
 def render_board(board):
@@ -19,7 +20,7 @@ def validate_user_input(guesses_left):
         user_input = input('Please enter a single letter:\n')
     else:
 
-        return user_input
+        return user_input.lower()
 
 
 def build_board(word):
@@ -38,19 +39,21 @@ def validate_board(user_guess, board):
 
     for item in board:
         letters.append(item[0])
+
     if user_guess not in letters:
         invalid_guess = True
     return invalid_guess
 
 
 def play_game():
-    with open('./test-word.txt') as open_file:
-        read_file = open_file.read()
-        guess_word = read_file.strip()
-        board = build_board(guess_word)
+    with open('./words.txt') as open_file:
+        read_file = open_file.readlines()
+        random_word = read_file[random.randint(0, len(read_file))]
+        print(random_word)
+        board = build_board(random_word.strip().lower())
         guesses_left = 8
 
-        print(f'The magic word contains {len(guess_word)} letters!')
+        print(f'The magic word contains {len(random_word)} letters!')
         render_board(board)
 
         user_guess = validate_user_input(guesses_left)
@@ -69,7 +72,7 @@ def play_game():
             if invalid_guess:
                 print('Wrong guess')
                 guesses_left = guesses_left - 1 
-        print(f'You lost, you ran out of guesses! correct word was: {guess_word}')
+        print(f'You lost, you ran out of guesses! correct word was: {random_word}')
 
 if __name__ == "__main__":
     play_game()
