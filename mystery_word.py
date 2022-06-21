0da06f7fc372cdd5927fcb4ec47b29b69dc434da
andres alcocer <devandres.tech@gmail.com>
2022-06-21 12:50:55 -0400

    set basic input logic

--- mystery_word.py
+++ mystery_word.py
@@ -1,6 +1,30 @@
+def render_board(word):
+    board = ['_' for letter in word]
+    print(' '.join(blank for blank in board))
+    print('\n')
+
+
+def validate_user_input():
+    user_input = input('Make your guess:\n')
+    while len(user_input) > 1:
+        user_input = input('Please enter a single letter:\n')
+    else:
+        return user_input
+
+
 def play_game():
-    pass
+    with open('./test-word.txt') as open_file:
+        read_file = open_file.read()
+        guess_word = read_file
+        print(f'The magic word contains {len(guess_word)} letters!')
+        render_board(guess_word)
+
+        user_guess = validate_user_input()
+        while user_guess != 'y':
+            render_board(guess_word)
+            user_guess = validate_user_input()
 
 
 if __name__ == "__main__":
     play_game()
+
